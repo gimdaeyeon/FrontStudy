46119c1138ff38e16d9e937309e21d6d8a32994d
gimdaeyeon <rlaeodus1306@naver.com>
2024-07-21 21:23:47 +0900

    vue 학습용 디렉토리 생성

---
+++ vue/study1/src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+  import {ref} from 'vue';
+
+  const cnt = ref(0);
+  const title =ref('vue start');
+  // const isTrue = ref(true);
+
+</script>
+
+<template>
+  <div>
+      <h1>{{ title }}</h1>
+      <p>{{ cnt }}</p>
+      <button @click="cnt++">+</button>
+      <button @click="cnt--">-</button>
+      <input type="text" v-model="title" >
+      <button :disabled="true">하하하하하하하</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
