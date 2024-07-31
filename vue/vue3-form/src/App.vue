fd60fb5c9ed16892aff1382516b3d7e4ee970b4b
gimdaeyeon <rlaeodus1306@naver.com>
2024-07-31 23:28:16 +0900

    Study: vue axios사용한 api 요청 compsition API간단한 사용

---
+++ vue/vue3-form/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <form action="" @submit.prevent="submitForm">
+    <div>
+      <label for="userId">ID</label>
+      <input type="text" id="userId" v-model="username" />
+    </div>
+    <div>
+      <label for="password">PW</label>
+      <input type="text" id="passoword" v-model="password" />
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+import {ref} from 'vue';
+
+export default {
+  setup(){
+    const username = ref('');
+    const password = ref('');
+
+    const submitForm= ()=>{
+      axios.post('https://jsonplaceholder.typicode.com/users',{
+        username:username.value,
+        password: password.value
+      })
+      .then(console.log);
+    }
+
+    return {username,password,submitForm}
+  }
+};
+</script>
+
+<style scoped></style>
