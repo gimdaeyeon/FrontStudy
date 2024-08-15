d31769f37e5300be42c56eb00eb72bfb970628ce
gimdaeyeon <rlaeodus1306@naver.com>
2024-08-16 00:14:03 +0900

    Study: 할일 관리 헤더, 인풋 컴포넌트 구현

---
+++ vue/vue3-todo/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <TodoHeader/>
+  <TodoInput/>
+<!--  TodoList-->
+</template>
+
+<script>
+
+import TodoHeader from "@/components/TodoHeader.vue";
+import TodoInput from "@/components/TodoInput.vue";
+
+export default {
+  components: {
+    TodoInput,
+    TodoHeader,
+
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
