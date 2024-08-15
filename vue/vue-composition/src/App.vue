e2f05d7bf0a18f5461c43471f8a89236d9769768
gimdaeyeon <rlaeodus1306@naver.com>
2024-08-15 23:07:21 +0900

    Study: 컴포지션 API 기본 코드 작성

---
+++ vue/vue-composition/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>{{message}}</p>
+    <button @click="changeMessage">클릭</button>
+  </div>
+</template>
+
+<script>
+
+import {ref} from "vue";
+
+export default {
+  setup(){
+    const message = ref('hello');
+
+    function changeMessage(){
+      message.value = 'hi';
+    }
+
+    return {message,changeMessage};
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
