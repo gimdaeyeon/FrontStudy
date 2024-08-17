6ac5d789f5d0153c6631d9221938e07c9829bd94
gimdaeyeon <rlaeodus1306@naver.com>
2024-08-17 14:22:30 +0900

    Study: 할일 관리 리스트 컴포넌트 구현

--- vue/vue3-todo/src/App.vue
+++ vue/vue3-todo/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <TodoHeader/>
   <TodoInput/>
-<!--  TodoList-->
+  <TodoList></TodoList>
 </template>
 
 <script>
 
 import TodoHeader from "@/components/TodoHeader.vue";
 import TodoInput from "@/components/TodoInput.vue";
+import TodoList from "@/components/TodoList.vue";
 
 export default {
   components: {
+    TodoList,
     TodoInput,
     TodoHeader,
 
--- vue/vue3-todo/src/components/TodoList.vue
+++ vue/vue3-todo/src/components/TodoList.vue
@@ -1,11 +1,21 @@
-<script setup>
-
-</script>
-
 <template>
-
+  <ul>
+    <li v-for="(item,index) in items" :key="index" >{{item}}</li>
+  </ul>
 </template>
 
-<style scoped>
+<script>
+import {ref} from "vue";
+
+export default {
+  setup(){
+    const items = ref(['애플','삼성','인프런']);
+
+    return {items}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
 
 </style>
