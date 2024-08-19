35210de654a8b01fe0cb80df25ca39b390c23d61
gimdaeyeon <rlaeodus1306@naver.com>
2024-08-19 22:25:31 +0900

    Study: redirect 속성과 router-link

---
+++ vue/vue-news/src/components/ToolBar.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-link to="/news">News</router-link>
+  <router-link to="/ask">Ask</router-link>
+  <router-link to="/jobs">Jobs</router-link>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+</style>
