0115d934b3bd76ee2ee410b896903135a1e7e674
gimdaeyeon <rlaeodus1306@naver.com>
2024-08-22 23:19:41 +0900

    Study: 뉴스 리스트 스타일링

--- vue/vue-news/src/App.vue
+++ vue/vue-news/src/App.vue
@@ -20,6 +20,17 @@ body {
   margin: 0;
   padding: 0;
 }
+a{
+  color: #34495e;
+  text-decoration: none;
+}
+a:hover{
+  color: #42b883;
+  text-decoration: underline;
+}
+a.router-link-exact-active{
+  text-decoration: underline;
+}
 /*Router Trasition*/
 .page-enter-active, .page-leave-active {
   transition: opacity 0.5s ease;
