c8d24216a0905c3972b5c9b9f8ab045907781d71
gimdaeyeon <rlaeodus1306@naver.com>
2024-09-17 18:11:39 +0900

    Study: 타입스크립트 기본 타입 학습

---
+++ typeScript/class-note/1_type-basic.ts
@@ -0,0 +1,32 @@
+// js 문자열 선언
+// let str = 'hello';
+
+// TS 문자열 선언
+let str: string = 'hello';
+
+// ts 숫자
+let num: number = 10;
+
+// ts 배열
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ['캡틴', '토르', '헐크'];
+let items: number[] = [1, 2, 3];
+
+// ts 튜플
+let address: [string, number] = ['강남', 12,];
+
+// ts 객체
+let obj: object = {};
+// let person: object = {
+//     name:'capt',
+//     age:100,
+// };
+
+let person: { name: string, age: number } = {
+    name: 'thor',
+    age: 1000,
+}
+
+// ts 진위값
+let show: boolean = true;
+
