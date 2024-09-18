d544ec1fb3bd8c990a8f1dd60305e5667d678a68
gimdaeyeon <rlaeodus1306@naver.com>
2024-09-18 21:40:53 +0900

    Study: 타입별칭, 인터페이스와 type차이점 학습

---
+++ typeScript/class-note/4_type-aliases.ts
@@ -0,0 +1,21 @@
+// interface Person{
+//     name:string;
+//     age:number;
+// }
+
+type Person={
+    name:string;
+    age:number;
+}
+
+const seho:Person={
+    name:'세호',
+    age:30
+}
+
+type MyString = string;
+const str:MyString = 'hello';
+
+
+
+
