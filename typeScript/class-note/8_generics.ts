45ccc51d9142755a02ddaa3a975730319383976e
gimdaeyeon <rlaeodus1306@naver.com>
2024-09-21 01:16:07 +0900

    Study: TypeScript 제네릭을 활용한 타입 제한 학습

--- typeScript/class-note/8_generics.ts
+++ typeScript/class-note/8_generics.ts
@@ -54,7 +54,50 @@ interface Dropdown<T> {
     selected: boolean;
 }
 
-const obc:Dropdown<string> = {value:'abc',selected:false}
+const obc: Dropdown<string> = {value: 'abc', selected: false}
+
+// // 제네릭의 타입제한
+// function logTextLength<T>(text: T[]): T[] {
+//     console.log(text.length);
+//     text.forEach(console.log);
+//     return text;
+// }
+//
+// logTextLength<string>(['hi', 'abc']);
+
+// 제네릭 타입 제한 2 - 저의된 타입 이용하기
+interface LengthType {
+    length: number;
+}
+
+function logTextLength<T extends LengthType>(text:T):T{
+    text.length;
+    return text;
+}
+
+// logTextLength(13);
+// logTextLength({length:3});
+
+// 제네릭 타입 제한 3 -keyof
+interface ShoppingItem{
+    name:string;
+    price:number;
+    stock:number;
+}
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption:T):T{
+    return itemOption;
+}
+// getShoppingItemOption(10);
+// getShoppingItemOption<string>('a');
+let a= 'price';
+getShoppingItemOption('price');
+// getShoppingItemOption(a);
+
+
+
+
+
+
 
 
 
