205f224924ef178a086b5dcc3966b0ef767f4e19
gimdaeyeon <rlaeodus1306@naver.com>
2024-12-17 23:45:44 +0900

    Study: React 이벤트 처리, state 학습

--- react/section04/src/App.jsx
+++ react/section04/src/App.jsx
@@ -1,19 +1,23 @@
 import './App.css'
 
-import React from 'react';
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button.jsx";
+import React, {useState} from 'react';
 
 function App() {
+    const [count, setCount] = useState(0);
+    const [light, setLight] = useState("OFF")
+
     return (
         <>
-            <Button text={"메일"} color={"red"}/>
-            <Button text={"카페"} />
-            <Button text={"블로그"} >
-                <Header/>
-            </Button>
+            <div>
+                <h1>{light}</h1>
+                <button onClick={()=>setLight(light === 'ON'?'Off':'ON')}>
+                    전구 {light === 'ON'?'끄기':'켜기'}
+                </button>
+            </div>
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count + 1)}
+            >+
+            </button>
         </>
     )
 }
--- react/section04/src/components/Button.jsx
+++ react/section04/src/components/Button.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 
 const Button = ({text, color, children}) => {
+
+    // 이벤트 객체
+    function onClickButton(e){
+        console.log(e);
+        console.log(text);
+    }
+
     return (
-        <button style={{color}}>
+        <button
+            onClick={onClickButton}
+            style={{color}}>
             {text} - {color.toUpperCase()}
             {children}
         </button>
