88997817288b7419e510c713ef8cef0a852f12f8
gimdaeyeon <rlaeodus1306@naver.com>
2024-12-18 23:04:00 +0900

    Study: React state, props 학습

--- react/section04/src/App.jsx
+++ react/section04/src/App.jsx
@@ -1,23 +1,18 @@
 import './App.css'
 
 import React, {useState} from 'react';
+import Bulb from "./components/Bulb.jsx";
+import Counter from "./components/Counter.jsx";
+
+
+
 
 function App() {
-    const [count, setCount] = useState(0);
-    const [light, setLight] = useState("OFF")
 
     return (
         <>
-            <div>
-                <h1>{light}</h1>
-                <button onClick={()=>setLight(light === 'ON'?'Off':'ON')}>
-                    전구 {light === 'ON'?'끄기':'켜기'}
-                </button>
-            </div>
-            <h1>{count}</h1>
-            <button onClick={() => setCount(count + 1)}
-            >+
-            </button>
+            <Bulb/>
+            <Counter/>
         </>
     )
 }
