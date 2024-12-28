63f8ea750a7c2010aaafe2d996e759a22241ad65
gimdaeyeon <rlaeodus1306@naver.com>
2024-12-28 21:06:15 +0900

    Study: React useEffect 개념학습

--- react/counter/src/App.jsx
+++ react/counter/src/App.jsx
@@ -1,10 +1,18 @@
 import Viewer from "./components/Viewer.jsx";
 import Controller from "./components/Controller.jsx";
 import "./App.css"
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function App() {
     const [count, setCount] = useState(0);
+    const [input, setInput] = useState("");
+
+    useEffect(() => {
+        console.log(`count: ${count}`);
+        console.log(`input: ${input}`);
+    }, [count,input]);
+    // 의존성 배열
+    // dependency array -> deps
 
     const onClickButton = (value)=>{
         setCount(count + value);
@@ -13,6 +21,9 @@ function App() {
     return (
         <div className="app">
             <h1>Simple Counter</h1>
+            <section>
+                <input type="text" value={input} onChange={(e)=>{setInput(e.target.value)}}/>
+            </section>
             <section>
                 <Viewer count={count}/>
             </section>
