d12931be09b99e007c8886b07603e7243b3bd661
gimdaeyeon <rlaeodus1306@naver.com>
2024-12-28 23:38:59 +0900

    Study: Vue + Ts 인풋 컴포넌트 상태 정의, 이벤트 정의 및 구현

--- vueType/vue-todo/src/App.vue
+++ vueType/vue-todo/src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
     <h1>Vue Todo with Typescript</h1>
-    <TodoInput/>
+    <h1>{{todoText}}</h1>
+    <TodoInput :item="todoText" @input="updateTodoText"/>
   </div>
 </template>
 
 <script setup lang="ts">
-
 import TodoInput from "@/components/TodoInput.vue";
+import {ref} from "vue";
+
+const todoText = ref('');
+
+function updateTodoText(text:any){
+  todoText.value = text;
+}
+
 </script>
 
 <style scoped>
--- vueType/vue-todo/src/components/TodoInput.vue
+++ vueType/vue-todo/src/components/TodoInput.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
     <label for="todo-input"></label>
-    <input id="todo-input" type="text">
+    <input id="todo-input" type="text" :value="item" @input="handleInput" />
     <button @clcik="addTodo" type="button">add</button>
   </div>
 </template>
 
 <script setup lang="ts">
 
+const props = defineProps(['item']);
+const emit = defineEmits(['input']);
+
 function addTodo(){
-  console.log('hello');
+  console.log(props.item);
+}
+
+function handleInput(e:any){
+  emit('input',e.target.value);
 }
 
 </script>
