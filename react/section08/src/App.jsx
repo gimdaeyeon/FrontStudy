8426f71b7ebe86fb99e976e9378d788d780d4280
gimdaeyeon <rlaeodus1306@naver.com>
2025-03-29 17:43:42 +0900

    Style: UI 구현

--- react/section08/src/App.jsx
+++ react/section08/src/App.jsx
@@ -1,13 +1,18 @@
 import {useState} from 'react'
 import './App.css'
+import Header from "./components/Header.jsx";
+import Editor from "./components/Editor.jsx";
+import List from "./components/List.jsx";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-        TodoList
-    </>
+    <div className="App">
+        <Header/>
+        <Editor/>
+        <List/>
+    </div>
   )
 }
 
