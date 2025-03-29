fbadc1775031eb465e21cda4ddcfb280717b88e1
gimdaeyeon <rlaeodus1306@naver.com>
2025-03-29 20:56:44 +0900

    Studh: [todo list 프로젝트] todo 추가하기

--- react/section08/src/App.jsx
+++ react/section08/src/App.jsx
@@ -1,19 +1,50 @@
-import {useState} from 'react'
 import './App.css'
 import Header from "./components/Header.jsx";
 import Editor from "./components/Editor.jsx";
 import List from "./components/List.jsx";
+import {useRef, useState} from "react";
 
+const mockData = [
+    {
+        id: 0,
+        isDone: false,
+        content: 'React 공부하기',
+        date: new Date().getTime(),
+    },
+    {
+        id: 1,
+        isDone: false,
+        content: 'Vue 공부하기',
+        date: new Date().getTime(),
+    },
+    {
+        id: 2,
+        isDone: false,
+        content: 'Spring 공부하기',
+        date: new Date().getTime(),
+    }
+]
 function App() {
-  const [count, setCount] = useState(0)
+    const [todos, setTodos] = useState(mockData);
+    const idRef = useRef(3);
 
-  return (
-    <div className="App">
-        <Header/>
-        <Editor/>
-        <List/>
-    </div>
-  )
+    const onCreate = (content)=>{
+        const newTodo = {
+            id:idRef.current++,
+            isDone: false,
+            content,
+            date: new Date().getTime(),
+        }
+        setTodos([newTodo,...todos]);
+    }
+
+    return (
+        <div className="App">
+            <Header/>
+            <Editor onCreate={onCreate}/>
+            <List/>
+        </div>
+    )
 }
 
 export default App
--- react/section08/src/components/Editor.jsx
+++ react/section08/src/components/Editor.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import './Editor.css'
 
-const Editor = () => {
+const Editor = ({onCreate}) => {
+    const [content, setContent] = useState('');
+    const contentRef = useRef(null);
+    const onSubmit = () => {
+        if (!content) {
+            alert('값을 입력해 주세요');
+            contentRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent('');
+    }
+
+    const onKeyDown = (e) => {
+        if (e.code.toLowerCase() === 'enter') {
+            onSubmit();
+        }
+    }
+
     return (
         <div className="Editor">
-            <input placeholder="새로운 Todo..."/>
-            <button>추가</button>
+            <input placeholder="새로운 Todo..." value={content} ref={contentRef}
+                   onInput={(e) => setContent(e.target.value)}
+                   onKeyDown={onKeyDown}
+            />
+            <button onClick={onSubmit}>추가</button>
         </div>
     );
 };
