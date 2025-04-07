74d88e432de8a6d9286face5d6797ee1459950e3
gimdaeyeon <rlaeodus1306@naver.com>
2025-04-07 21:44:59 +0900

    Feat: 일기 관리 기능 구현

--- react/diary/src/App.jsx
+++ react/diary/src/App.jsx
@@ -3,7 +3,7 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import New from "./pages/New";
 import Diary from "./pages/Diary";
-import React, {useReducer} from "react";
+import React, {createContext, useReducer, useRef} from "react";
 import Notfound from "./pages/Notfound";
 import Button from "./components/Button";
 import Header from "./components/Header";
@@ -24,26 +24,76 @@ const mockData = [
     },
 ]
 
-function reducer(state,action){
-    return state;
+function reducer(state, action) {
+    switch (action.type) {
+        case 'CREATE':
+            return [action.data, ...state];
+        case 'UPDATE':
+            return state.map(item =>
+                String(item.id) === String(action.data.id)
+                    ? action.data
+                    : item
+            );
+        case 'DELETE':
+            return state.filter(item => String(item.id) !== String(action.id));
+        default:
+            return state;
+    }
 }
 
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
 
 function App() {
-    const [data, dispatch] = useReducer(reducer,mockData);
+    const [data, dispatch] = useReducer(reducer, mockData);
+    const idRef = useRef(3);
+
+    // 새로운 일기 추가
+    const onCreate = (emotionId, content) => {
+        dispatch({
+            type: 'CREATE',
+            data: {
+                id: idRef.current++,
+                createdDate: new Date().getTime(),
+                emotionId,
+                content,
+            }
+        })
+    }
+
+    // 기존 일기 수정
+    const onUpdate = (id, emotionId, content) => {
+        dispatch({
+            type: 'UPDATE',
+            id, emotionId, content,
+            createdDate: new Date().getTime(),
+        })
+    }
+    // 기존 일기 삭제
+    const onDelete = (id) => {
+        dispatch({
+            type: 'DELETE', id
+        })
+    }
+
     return (
         <>
             <Header title={"Header"}
-                leftChild={<Button text={"<"}/>}
-                rightChild={<Button text={">"}/>}
+                    leftChild={<Button text={"<"}/>}
+                    rightChild={<Button text={">"}/>}
             />
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/new" element={<New/>}/>
-                <Route path="/diary/:id" element={<Diary/>}/>
-                <Route path="/edit/:id" element={<Edit/>}/>
-                <Route path="*" element={<Notfound/>}/>
-            </Routes>
+            <DiaryStateContext.Provider value={data}>
+                <DiaryDispatchContext.Provider value={{onCreate,onUpdate,onDelete}}>
+                    <Routes>
+                        <Route path="/" element={<Home/>}/>
+                        <Route path="/new" element={<New/>}/>
+                        <Route path="/diary/:id" element={<Diary/>}/>
+                        <Route path="/edit/:id" element={<Edit/>}/>
+                        <Route path="*" element={<Notfound/>}/>
+                    </Routes>
+                </DiaryDispatchContext.Provider>
+
+            </DiaryStateContext.Provider>
         </>
     )
 }
