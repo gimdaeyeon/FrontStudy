7a7542ba86100c99dfd867324999a52866231fa2
gimdaeyeon <rlaeodus1306@naver.com>
2025-10-04 17:08:05 +0900

    Study: 레이아웃 컴포넌트 NuxtLink 태그 관련 학습

--- nuxt/learn-nuxt/app/layouts/default.vue
+++ nuxt/learn-nuxt/app/layouts/default.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
-    <NuxtLink to="/">초기</NuxtLink>
-    <NuxtLink to="/main">메인</NuxtLink>
-    <NuxtLink to="/blog/hello">안녕</NuxtLink>
+    <div class="link-box">
+      <NuxtLink to="/">홈페이지</NuxtLink>
+      <NuxtLink to="/main">메인 페이지</NuxtLink>
+      <NuxtLink to="/product">상품 페이지</NuxtLink>
+      <NuxtLink to="/blog/hello">안녕</NuxtLink>
+    </div>
 
+    <h1>{{ $route.name}} 페이지</h1>
     <slot />
   </div>
 </template>
@@ -13,5 +17,9 @@
 </script>
 
 <style scoped>
-
+.link-box{
+  a{
+    margin-right: 4px;
+  }
+}
 </style>
