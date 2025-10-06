ee71bb00a248ddac1abd6976134cc53e9b869423
gimdaeyeon <rlaeodus1306@naver.com>
2025-10-06 23:12:26 +0900

    Study: nuxt 서버 포트 설정

--- nuxt/learn-nuxt/app/pages/main.vue
+++ nuxt/learn-nuxt/app/pages/main.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
     <p>메인 페이지 입니다.</p>
+    <div>
+      {{products}}
+    </div>
   </div>
 </template>
 
@@ -8,9 +11,12 @@
 import axios from "axios";
 import {onMounted} from "vue";
 
+const products = ref([]);
+
 onMounted(async ()=>{
   const response = await axios.get('http://localhost:3000/products');
   console.log(response);
+  products.value = response.data;
 })
 
 </script>
