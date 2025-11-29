a785a0f440055956e125bd26208bcae5ca6cc322
gimdaeyeon <rlaeodus1306@naver.com>
2025-11-29 15:14:10 +0900

    Study: next 프리페칭 관련 학습

--- next/obb/section02/src/pages/_app.tsx
+++ next/obb/section02/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import {useRouter} from "next/router";
+import {useEffect} from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
 
@@ -10,10 +11,14 @@ export default function App({ Component, pageProps }: AppProps) {
     router.push('/test');
   };
 
+  useEffect(() => {
+    router.prefetch('/test')
+  }, []);
+
   return <>
     <header>
       <Link href={"/"}>index</Link>&nbsp;
-      <Link href={"/search"}>search</Link>&nbsp;
+      <Link href={"/search"} prefetch={false}>search</Link>&nbsp;
       <Link href={"/book/1"}>book/1</Link>&nbsp;
       <div>
         <button onClick={onClickButton}>/test 페이지로 이동</button>
