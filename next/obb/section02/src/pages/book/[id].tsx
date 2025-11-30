b40adf16ce9560c0ba5a1223dc9d0fd4a63a7301
gimdaeyeon <rlaeodus1306@naver.com>
2025-11-30 20:55:35 +0900

    Study: Next 사전 렌더링, SSR관련 내용 학습

--- next/obb/section02/src/pages/book/[id].tsx
+++ next/obb/section02/src/pages/book/[id].tsx
@@ -13,7 +13,6 @@ const mockData = {
 
 export default function Page() {
   const {
-    id,
     title,
     subTitle,
     description,
--- next/obb/section02/src/pages/index.tsx
+++ next/obb/section02/src/pages/index.tsx
@@ -1,28 +1,48 @@
 import style from './index.module.css';
 import SearchableLayout from "@/components/searchable-layout";
-import {ReactNode} from "react";
+import {ReactNode, useEffect} from "react";
 import books from '@/mock/books.json'
 import BookItem from "@/components/book-item";
+import {InferGetServerSidePropsType} from "next";
 
-export default function Home() {
-    return (
-        <div className={style.container}>
-          <section>
-            <h3>지금 추천하는 도서</h3>
-            {books.map((book)=>(
-                <BookItem key={book.id} {...book} />
-            ))}
-          </section>
-          <section>
-            <h3>등록된 모든 도서</h3>
-            {books.map((book)=>(
-                <BookItem key={book.id} {...book} />
-            ))}
-          </section>
-        </div>
-    );
+export const getServerSideProps = () => {
+//   컴포넌트보다 먼저 실행되어서,
+//   컴포넌트에 필요한 데이터를 불러오는 함수
+  const data = 'hello';
+
+  return {
+    props: {
+      data,
+    }
+  }
+};
+
+export default function Home({
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+
+  console.log(data);
+
+  useEffect(() => {
+    console.log(window);
+  }, []);
+  return (
+      <div className={style.container}>
+        <section>
+          <h3>지금 추천하는 도서</h3>
+          {books.map((book) => (
+              <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+        <section>
+          <h3>등록된 모든 도서</h3>
+          {books.map((book) => (
+              <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+      </div>
+  );
 }
 
-Home.getLayout = (page:ReactNode)=>{
+Home.getLayout = (page: ReactNode) => {
   return <SearchableLayout>{page}</SearchableLayout>
 }
