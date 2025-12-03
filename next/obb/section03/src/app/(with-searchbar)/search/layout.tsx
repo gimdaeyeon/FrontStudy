a0746a383ab9dc0fa7f6a9831ebeb410cfe1d651
gimdaeyeon <rlaeodus1306@naver.com>
2025-12-03 14:54:01 +0900

    Study: App 라우터 네비게이팅 학습

--- next/obb/section03/src/app/(with-searchbar)/search/layout.tsx
+++ next/obb/section03/src/app/(with-searchbar)/search/layout.tsx
@@ -1,5 +1,5 @@
 import {ReactNode} from "react";
-import SearchBar from "@/app/(with-searchbar)/search/searchbar";
+import SearchBar from "@/app/components/searchbar";
 
 export default function Layout({children,}: { children: ReactNode }) {
     return (
