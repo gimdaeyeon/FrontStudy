8ae957fc4f97a3bd9f687d5e3e7956e592a9425d
gimdaeyeon <rlaeodus1306@naver.com>
2025-12-06 20:43:31 +0900

    Study: 리뷰 삭제 기능 구현

--- next/obb/section03/src/actions/create-review.action.ts
+++ next/obb/section03/src/actions/create-review.action.ts
@@ -2,12 +2,17 @@
 
 import {revalidatePath, revalidateTag} from "next/cache";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_:any,formData: FormData) {
     const bookId = formData.get('bookId')?.toString();
     const content = formData.get('content')?.toString();
     const author = formData.get('author')?.toString();
 
-    if (!bookId|| !content || !author) return;
+    if (!bookId|| !content || !author) {
+        return {
+            status: false,
+            error:'리뷰 내용과 작성자를 입력해 주세요'
+        }
+    }
 
     try {
         const response = await fetch(
@@ -16,6 +21,10 @@ export async function createReviewAction(formData: FormData) {
                 body: JSON.stringify({bookId, content, author}),
             });
         console.log(response.status);
+
+        if(!response.ok){
+            throw new Error(response.statusText);
+        }
 
         // 1. 특정 주소의 해당하는 페이지만 재검증
     //     revalidatePath(`/book/${bookId}`,); // 서버쪽에서만 동작하는 메서드
@@ -31,9 +40,15 @@ export async function createReviewAction(formData: FormData) {
 
     //     5. 태그 기준, 데이터 캐시 재검증
         revalidateTag(`review-${bookId}`);
-
-    } catch (e) {
-        console.error(e);
-        return;
+        return {
+            status:true,
+            error:'',
+        };
+    } catch (err) {
+        console.error(err);
+        return {
+            status: false,
+            error :`리뷰 저장에 실패했습니다 : ${err}`
+        }
     }
 }
