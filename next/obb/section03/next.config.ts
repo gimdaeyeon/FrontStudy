27c2e56e0f4324ea2e8b0a7d1ec2e67d59c7fa95
gimdaeyeon <rlaeodus1306@naver.com>
2025-12-07 21:13:49 +0900

    Study: Image 테그를 활용한 이미지 최적화 학습

--- next/obb/section03/next.config.ts
+++ next/obb/section03/next.config.ts
@@ -2,11 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    // logging:{
-    //     fetches:{
-    //         fullUrl:true,
-    //     },
-    // },
+    logging:{
+        fetches:{
+            fullUrl:true,
+        },
+    },
+    images:{
+        remotePatterns:[
+            {
+                protocol:'https',
+                hostname:'shopping-phinf.pstatic.net',
+                pathname:'**',
+            }
+        ]
+    }
 };
 
 export default nextConfig;
--- next/obb/section03/src/app/book/[id]/page.tsx
+++ next/obb/section03/src/app/book/[id]/page.tsx
@@ -3,6 +3,7 @@ import {BookData, ReviewData} from "@/types";
 import {notFound} from "next/navigation";
 import ReviewItem from "@/components/review/review-item";
 import ReviewEditor from "@/components/review/review-editor";
+import Image from "next/image";
 
 // generateStaticParams 로 반환된 url이외의 페이지를 동적으로 생성하지 않고 404페이지로 이동
 // export const dynamicParams = false;
@@ -34,7 +35,7 @@ async function BookDetail({bookId}:{bookId:string}){
                 className={style.cover_img_container}
                 style={{ backgroundImage: `url('${coverImgUrl}')` }}
             >
-                <img src={coverImgUrl} />
+                <Image src={coverImgUrl} width={240} height={300} alt={`도서 ${title}의 표지 이미지`} />
             </div>
             <div className={style.title}>{title}</div>
             <div className={style.subTitle}>{subTitle}</div>
