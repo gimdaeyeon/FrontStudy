75b7c15ddd19fc399c849d6c8722782ae1c6e95a
gimdaeyeon <rlaeodus1306@naver.com>
2025-12-29 22:23:29 +0900

    Study: tailwind css 핵심 클래스 학습

--- react/onbite-exam/src/App.tsx
+++ react/onbite-exam/src/App.tsx
@@ -3,8 +3,21 @@ import './App.css'
 function App() {
 
   return (
-    <div className="font-bold underline text-2xl bg-green-500">
-      Hello world
+    <div>
+        <div className="m-4"></div>
+    {/*   5. 보더*/}
+        <div className="m-5 border-3 border-orange-500 rounded-lg">
+            border
+        </div>
+
+    {/*    6. 플렉스 컨테이너*/}
+        <div className="flex justify-evenl">
+            <div className="w-10 border">1</div>
+            <div className="w-10 border flex-1">2</div>
+            <div className="w-10 border flex-2">3</div>
+            <div className="w-10 border">4</div>
+        </div>
+
     </div>
   )
 }
