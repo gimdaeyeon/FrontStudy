21d456beb394817fb677cdf67267bd34dd1712f6
gimdaeyeon <rlaeodus1306@naver.com>
2025-12-29 23:36:31 +0900

    Study: shadcn 핵심 ui input, toaster, textarea등 학습

--- react/onbite-exam/src/App.tsx
+++ react/onbite-exam/src/App.tsx
@@ -1,11 +1,30 @@
 import './App.css'
 import {Button} from "@/components/ui/button.tsx";
+import {Input} from "@/components/ui/input.tsx";
+import {Textarea} from "@/components/ui/textarea.tsx";
+import {Toaster} from "@/components/ui/sonner.tsx";
+import {toast} from "sonner";
 
 function App() {
 
   return (
-    <div>
-        <Button className="cursor-pointer" >버튼</Button>
+    <div className="p-5">
+        <Toaster/>
+        <Textarea/>
+        <Input placeholder="입력..."/>
+        <Button className="cursor-pointer"
+                onClick={() => {
+                    toast('ㅋㅋㅋㅋㅋ',{
+                        position: 'top-center'
+                    })
+                }}
+        >버튼</Button>
+        <Button variant={"destructive"} >버튼</Button>
+        <Button variant={"ghost"} >버튼</Button>
+        <Button variant={"link"} >버튼</Button>
+        <Button variant={"outline"} >버튼</Button>
+        <Button variant={"secondary"} >버튼</Button>
+
     </div>
   )
 }
