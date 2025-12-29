c8e9ea825f99c0804a5e589335814b3b732c5b8b
gimdaeyeon <rlaeodus1306@naver.com>
2025-12-29 23:20:51 +0900

    Study: shadcn ui 설치

--- react/onbite-exam/src/App.tsx
+++ react/onbite-exam/src/App.tsx
@@ -1,23 +1,11 @@
 import './App.css'
+import {Button} from "@/components/ui/button.tsx";
 
 function App() {
 
   return (
     <div>
-        <div className="m-4"></div>
-    {/*   5. 보더*/}
-        <div className="m-5 border-3 border-orange-500 rounded-lg">
-            border
-        </div>
-
-    {/*    6. 플렉스 컨테이너*/}
-        <div className="flex justify-evenl">
-            <div className="w-10 border">1</div>
-            <div className="w-10 border flex-1">2</div>
-            <div className="w-10 border flex-2">3</div>
-            <div className="w-10 border">4</div>
-        </div>
-
+        <Button className="cursor-pointer" >버튼</Button>
     </div>
   )
 }
