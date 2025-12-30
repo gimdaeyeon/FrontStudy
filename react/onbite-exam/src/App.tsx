78d83f05856716e681e050649b8e105109d5f6d8
gimdaeyeon <rlaeodus1306@naver.com>
2025-12-30 22:54:38 +0900

    Study: shadcn 핵심 ui 학습2

--- react/onbite-exam/src/App.tsx
+++ react/onbite-exam/src/App.tsx
@@ -4,11 +4,70 @@ import {Input} from "@/components/ui/input.tsx";
 import {Textarea} from "@/components/ui/textarea.tsx";
 import {Toaster} from "@/components/ui/sonner.tsx";
 import {toast} from "sonner";
+import {Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious} from "@/components/ui/carousel.tsx";
+import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover.tsx";
+import {Dialog, DialogContent, DialogDescription, DialogHeader, DialogTrigger} from "@/components/ui/dialog.tsx";
+import {DialogTitle} from "@radix-ui/react-dialog";
+import {
+    AlertDialog, AlertDialogAction, AlertDialogCancel,
+    AlertDialogContent, AlertDialogDescription, AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger
+} from "@/components/ui/alert-dialog.tsx";
+import { Activity } from 'lucide-react';
 
 function App() {
 
   return (
     <div className="p-5">
+        <Activity className="size-10" color="red" />
+        <AlertDialog>
+            <AlertDialogTrigger>Open</AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogTitle>Title</AlertDialogTitle>
+                <div>Body</div>
+                <div>
+                    <AlertDialogAction>Continue</AlertDialogAction>
+                    <AlertDialogCancel>Cancel</AlertDialogCancel>
+                </div>
+            </AlertDialogContent>
+        </AlertDialog>
+
+        <Dialog>
+            <DialogTrigger>Open Dialog</DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Dialog Title</DialogTitle>
+                    <DialogDescription>Dialog Description</DialogDescription>
+                    <div>Body</div>
+                </DialogHeader>
+            </DialogContent>
+        </Dialog>
+
+        <Popover>
+            <PopoverTrigger asChild >
+                <Button>Open</Button>
+            </PopoverTrigger>
+
+            <PopoverContent>
+                <Button>팝오버 버튼</Button>
+                <Button>팝오버 버튼2</Button>
+            </PopoverContent>
+        </Popover>
+        
+        <Carousel className="mx-10">
+            <CarouselContent>
+                <CarouselItem className="basis-1/3">1</CarouselItem>
+                <CarouselItem className="basis-1/3">2</CarouselItem>
+                <CarouselItem className="basis-1/3">3</CarouselItem>
+                <CarouselItem className="basis-1/3">4</CarouselItem>
+                <CarouselItem className="basis-1/3">5</CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious  />
+            <CarouselNext/>
+        </Carousel>
+
         <Toaster/>
         <Textarea/>
         <Input placeholder="입력..."/>
