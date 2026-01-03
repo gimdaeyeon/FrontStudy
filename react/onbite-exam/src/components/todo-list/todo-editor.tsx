94255faf85d4beacdd3ee464d6389f6a245375a4
gimdaeyeon <rlaeodus1306@naver.com>
2026-01-03 18:04:41 +0900

    Study: zustand를 사용한 투두리스트 기능 구현

--- react/onbite-exam/src/components/todo-list/todo-editor.tsx
+++ react/onbite-exam/src/components/todo-list/todo-editor.tsx
@@ -1,10 +1,28 @@
 import {Input} from "@/components/ui/input.tsx";
 import {Button} from "@/components/ui/button.tsx";
+import {useState} from "react";
+import {useCreateTodo} from "@/store/todos.ts";
 
 export default function TodoEditor() {
+    const createTodo = useCreateTodo();
+    const [content, setContent] = useState('');
+
+    const handleAddTodo = ()=>{
+        if(content.trim() === '') return;
+        createTodo(content);
+        setContent('');
+    }
+
     return (
         <div className="flex gap-2">
-            <Input placeholder="새로운 할 일을 입력하세요..."/>
-            <Button className="cursor-pointer">추가</Button>
+            <Input value={content} placeholder="새로운 할 일을 입력하세요..."
+                   onChange={(e) => setContent(e.target.value)}
+                   onKeyDown={(e) => e.key === 'Enter' && handleAddTodo() }
+            />
+            <Button className="cursor-pointer"
+                    onClick={handleAddTodo}
+            >
+                추가
+            </Button>
         </div>)
 }
--- react/onbite-exam/src/pages/todo-list-page.tsx
+++ react/onbite-exam/src/pages/todo-list-page.tsx
@@ -1,20 +1,16 @@
 import TodoEditor from "@/components/todo-list/todo-editor.tsx";
 import TodoItem from "@/components/todo-list/todo-item.tsx";
-
-const dummyTodos = [
-    {id:1, content:"할일1"},
-    {id:2, content:"할일2"},
-    {id:3, content:"할일3"},
-]
+import {useTodos} from "@/store/todos.ts";
 
 export default function TodoListPage() {
+    const todos = useTodos();
     return (
         <div className="flex flex-col gap-5 p-5">
             <h1 className="font-bold text-2xl">Todo List</h1>
             <TodoEditor/>
-            {dummyTodos.map(todo=>(
+            {todos.map(todo=>(
                 <TodoItem key={todo.id} {...todo}/>
             ))}
         </div>
-    )
+    );
 }
