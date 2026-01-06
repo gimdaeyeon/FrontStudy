317c2508b1c84b7804da0e4fb20da9747717fa36
gimdaeyeon <rlaeodus1306@naver.com>
2026-01-06 22:54:59 +0900

    Study: [캐시 데이터 다루기] - 응답 결과값 이용한 화면 렌더링 학습

--- react/onbite-exam/src/hooks/mutations/use-create-todo-mutation.ts
+++ react/onbite-exam/src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,19 +1,21 @@
 import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {createTodo} from "@/api/create-todo.ts";
 import {QUERY_KEYS} from "@/lib/constants.ts";
+import type {Todo} from "@/types.ts";
 
 export function useCreateTodoMutation() {
     const queryClient = useQueryClient();
     return useMutation({
-        mutationFn:createTodo,
-        onMutate: ()=>{},
-        onSettled: ()=>{},
-        onSuccess: ()=>{
-            queryClient.invalidateQueries({
-                queryKey:QUERY_KEYS.todo.list,
-            })
+        mutationFn: createTodo,
+        onMutate: () => {},
+        onSettled: () => {},
+        onSuccess: (newTodo) => {
+            queryClient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+                if(!prevTodos) return [newTodo];
+                return [...prevTodos,newTodo];
+            });
         },
-        onError: (error)=>{
+        onError: (error) => {
             alert(error.message);
         },
     });
