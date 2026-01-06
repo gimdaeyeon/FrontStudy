5d9b5b2e02b113872528c20c92f1aba27b1670ca
gimdaeyeon <rlaeodus1306@naver.com>
2026-01-06 22:40:07 +0900

    Study: 캐시 데이터 무효화로 데이터 refetching, queyrFactory 패턴으로 캐시 무효화 관련 학습

--- react/onbite-exam/src/hooks/mutations/use-create-todo-mutation.ts
+++ react/onbite-exam/src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,13 +1,17 @@
-import {useMutation} from "@tanstack/react-query";
+import {useMutation, useQueryClient} from "@tanstack/react-query";
 import {createTodo} from "@/api/create-todo.ts";
+import {QUERY_KEYS} from "@/lib/constants.ts";
 
 export function useCreateTodoMutation() {
+    const queryClient = useQueryClient();
     return useMutation({
         mutationFn:createTodo,
         onMutate: ()=>{},
         onSettled: ()=>{},
         onSuccess: ()=>{
-            window.location.reload();
+            queryClient.invalidateQueries({
+                queryKey:QUERY_KEYS.todo.list,
+            })
         },
         onError: (error)=>{
             alert(error.message);
