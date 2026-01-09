11206f991b9765058292ba34dadc745fcceb8106
gimdaeyeon <rlaeodus1306@naver.com>
2026-01-09 23:29:41 +0900

    Study: 실습프로젝트 글로벌 레이아웃 설정

---
+++ react/onebite-log/src/components/layout/global-layout.tsx
@@ -0,0 +1,31 @@
+import {Link, Outlet} from "react-router";
+import logo from '@/assets/logo.png'
+import defaultAvatar from '@/assets/default-avatar.jpg'
+import {SunIcon} from "lucide-react";
+
+export default function GlobalLayout(){
+    return (
+        <div className="min-h-[100vh] flex flex-col">
+            <header className="h-15 border-b">
+                <div className="flex justify-between h-full max-w-175 w-full m-auto px-4">
+                    <Link to={'/'} className="flex items-center gpa-2">
+                        <img className="h-5" src={logo} alt="한입 로그의 로고, 메세지 말풍선을 형상화한 모양이다."/>
+                        <div className="font-bold">한입 로그</div>
+                    </Link>
+                    <div className="flex items-center gap-5">
+                        <div className="hover:bg-muted cursor-pointer rounded-full p-2">
+                            <SunIcon/>
+                        </div>
+                        <img className="h-6" src={defaultAvatar} alt=""/>
+                    </div>
+                </div>
+            </header>
+            <main className="m-auto max-w-175 w-full px-4 py-6 border-x flex-1">
+                <Outlet />
+            </main>
+            <footer className="border-t text-muted-foreground py-10 text-center">
+                @winterlood
+            </footer>
+        </div>
+    )
+}
