c83c79a63ef97d6ed2c9b2bcac28164aa667eecd
gimdaeyeon <rlaeodus1306@naver.com>
2026-01-26 23:01:18 +0900

    Feat: 좋아요 결과 반영하기 (낙관적 업데이트)

--- react/onebite-log/src/hooks/mutations/post/use-toggle-post-like.ts
+++ react/onebite-log/src/hooks/mutations/post/use-toggle-post-like.ts
@@ -1,15 +1,43 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { togglePostLike } from "@/api/post.ts";
-import type { UseMutationCallback } from "@/types.ts";
+import type { Post, UseMutationCallback } from "@/types.ts";
+import { QUERY_KEYS } from "@/lib/constants.ts";
 
-export default function useTogglePostLike(callbacks?:UseMutationCallback){
+export default function useTogglePostLike(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: togglePostLike,
-    onSuccess:(data)=>{
-      if(callbacks?.onSuccess) callbacks.onSuccess();
+    onMutate: async ({ postId }) => {
+      await queryClient.cancelQueries({
+        queryKey: QUERY_KEYS.post.byId(postId),
+      });
+
+      const prevPost = queryClient.getQueryData<Post>(
+        QUERY_KEYS.post.byId(postId),
+      );
+
+      queryClient.setQueryData<Post>(QUERY_KEYS.post.byId(postId), (post) => {
+        if (!post) throw new Error("포스트가 존재하지 않습니다.");
+        return {
+          ...post,
+          isLiked: !post.isLiked,
+          like_count: post.isLiked ? post.like_count - 1 : post.like_count + 1,
+        };
+      });
+
+      return { prevPost };
     },
-    onError: (error)=>{
-      if(callbacks?.onError) callbacks.onError(error);
-    }
-  })
+    onSuccess: (data) => {
+      if (callbacks?.onSuccess) callbacks.onSuccess();
+    },
+    onError: (error, _, context) => {
+      if (context && context.prevPost) {
+        queryClient.setQueryData(
+          QUERY_KEYS.post.byId(context.prevPost.id),
+          context.prevPost,
+        );
+      }
+      if (callbacks?.onError) callbacks.onError(error);
+    },
+  });
 }
