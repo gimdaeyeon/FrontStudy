2de6ff3a7b79da07ae6c9049b9774e3a7c6a1b2c
gimdaeyeon <rlaeodus1306@naver.com>
2026-01-27 23:48:38 +0900

    Feat: 프로필 버튼 구현하기

---
+++ react/onebite-log/src/components/layout/header/profile-button.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import defaultAvatar from "@/assets/default-avatar.jpg";
+import { useSession } from "@/store/session.ts";
+import { useProfileData } from "@/hooks/queries/use-profile-data.ts";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover.tsx";
+import { PopoverClose } from "@radix-ui/react-popover";
+import { Link } from "react-router";
+import { signOut } from "@/api/auth.ts";
+
+export default function ProfileButton() {
+  const session = useSession();
+  const { data: profile } = useProfileData(session?.user.id);
+
+  if (!session) return null;
+
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <img
+          src={profile?.avatar_url || defaultAvatar}
+          className="size-6 cursor-pointer rounded-full object-cover"
+        />
+      </PopoverTrigger>
+      <PopoverContent className="flex w-40 flex-col p-0">
+        <PopoverClose asChild>
+          <Link to={`/profile/${session.user.id}`}>
+            <div className="hover:bg-muted cursor-pointer px-4 py-3 text-sm">
+              프로필
+            </div>
+          </Link>
+        </PopoverClose>
+        <PopoverClose asChild>
+          <div onClick={signOut} className="hover:bg-muted cursor-pointer px-4 py-3 text-sm">
+            로그아웃
+          </div>
+        </PopoverClose>
+      </PopoverContent>
+    </Popover>
+  );
+}
