30c79121e597ba19e2c2708f8fc1faa317de4be6
gimdaeyeon <rlaeodus1306@naver.com>
2026-02-10 23:34:51 +0900

    feat: 댓글 수정 기능 구현

---
+++ react/onebite-log/src/hooks/mutations/comment/use-update-comment.ts
@@ -0,0 +1,15 @@
+import type { UseMutationCallback } from "@/types.ts";
+import { useMutation } from "@tanstack/react-query";
+import { updateComment } from "@/api/comment.ts";
+
+export function useUpdateComment(callbacks?: UseMutationCallback) {
+  return useMutation({
+    mutationFn: updateComment,
+    onSuccess: () => {
+      if (callbacks?.onSuccess) callbacks.onSuccess();
+    },
+    onError: (error) => {
+      if (callbacks?.onError) callbacks.onError(error);
+    },
+  });
+}
