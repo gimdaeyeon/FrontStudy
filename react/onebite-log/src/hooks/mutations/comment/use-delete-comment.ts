242c5fa8f702aae1525ce0de674becc0bf7d5245
gimdaeyeon <rlaeodus1306@naver.com>
2026-02-11 22:42:49 +0900

    feat: 캐시 수정 기능 추가

--- react/onebite-log/src/hooks/mutations/comment/use-delete-comment.ts
+++ react/onebite-log/src/hooks/mutations/comment/use-delete-comment.ts
@@ -1,12 +1,21 @@
-import type { UseMutationCallback } from "@/types.ts";
-import { useMutation } from "@tanstack/react-query";
+import type { Comment, UseMutationCallback } from "@/types.ts";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteComment } from "@/api/comment.ts";
+import { QUERY_KEYS } from "@/lib/constants.ts";
 
 export function useDeleteComment(callbacks?: UseMutationCallback) {
+  const queryClient = useQueryClient();
   return useMutation({
     mutationFn: deleteComment,
-    onSuccess: () => {
+    onSuccess: (deletedComment) => {
       if (callbacks?.onSuccess) callbacks.onSuccess();
+      queryClient.setQueryData<Comment[]>(
+        QUERY_KEYS.comment.post(deletedComment.post_id),
+        (comments)=>{
+          if(!comments) throw new Error('댓글이 캐시 데이터에 보관되어 있지 않습니다.');
+          return comments.filter(comment=>comment.id!== deletedComment.id);
+        },
+      )
     },
     onError: (error) => {
       if (callbacks?.onError) callbacks.onError(error);
