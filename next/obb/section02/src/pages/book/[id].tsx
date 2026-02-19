48767e6c6cf77744737f76c1f95d46072d3f4722
gimdaeyeon <rlaeodus1306@naver.com>
2026-02-20 00:16:24 +0900

    study: next 네비게이팅 학습

--- next/obb/section02/src/pages/book/[id].tsx
+++ next/obb/section02/src/pages/book/[id].tsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import {useRouter} from "next/router";
 
 export default function Page() {
-  const router = useRouter();
-  const {id} = router.query;
+  // const router = useRouter();
+  // const {id} = router.query;
   return (
     <div>
       <h1>Book</h1>
