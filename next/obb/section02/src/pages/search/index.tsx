b51367dfe4f0114b4e89c244144c875ad6ee8319
gimdaeyeon <rlaeodus1306@naver.com>
2026-02-28 01:01:49 +0900

    study: 한입북스 UI 구현

--- next/obb/section02/src/pages/search/index.tsx
+++ next/obb/section02/src/pages/search/index.tsx
@@ -1,15 +1,14 @@
 import React, {ReactNode} from 'react';
-import {useRouter} from "next/router";
 import SearchableLayout from "@/components/searchable-layout";
+import books from '@/mock/books.json';
+import BookItem from "@/components/book-item";
 
 export default function Page() {
-  const router = useRouter();
-
-  const {q} = router.query;
-
   return (
     <div>
-      <h1>Search {q}</h1>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
     </div>
   );
 };
