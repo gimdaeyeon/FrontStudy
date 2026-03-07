8c6e81c8bbfbf08827474118596bbcbd51b7b36f
gimdaeyeon <rlaeodus1306@naver.com>
2026-03-08 00:17:38 +0900

    study: SEO 설정

--- next/obb/section02/src/pages/index.tsx
+++ next/obb/section02/src/pages/index.tsx
@@ -5,6 +5,7 @@ import BookItem from "@/components/book-item";
 import {InferGetStaticPropsType} from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
+import Head from 'next/head';
 
 // export const getServerSideProps = async () => {
 export const getStaticProps = async () => {
@@ -22,20 +23,28 @@ export const getStaticProps = async () => {
 // export default function Home({allBooks, recoBooks}: InferGetServerSidePropsType<typeof getServerSideProps>) {
 export default function Home({allBooks, recoBooks}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {recoBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {allBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>한입북스</title>
+        <meta property="og:image" content={'/thumbnail.png'} />
+        <meta property="og:title" content="한입북스" />
+        <meta property="og:description" content="한입 북스에 등록된 도서들을 만나보세요" />
+      </Head>
+      <div className={style.container}>
+        <section>
+          <h3>지금 추천하는 도서</h3>
+          {recoBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+        <section>
+          <h3>등록된 모든 도서</h3>
+          {allBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+      </div>
+    </>
   );
 }
 
